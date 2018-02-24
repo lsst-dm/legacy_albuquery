a5d6036f384ab24cbabed10099b738e7ed95658d
Brian Van Klaveren <bvan@slac.stanford.edu>
2018-02-24 13:26:50 -0800

    Refactor based on Dropwizard and new features

--- src/main/kotlin/org/lsst/dax/albuquery/AlbuqueryConfiguration.kt
+++ src/main/kotlin/org/lsst/dax/albuquery/AlbuqueryConfiguration.kt
@@ -1,2 +1,17 @@
 package org.lsst.dax.albuquery
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.dropwizard.Configuration
+import io.dropwizard.db.DataSourceFactory
+
+class AlbuqueryConfiguration(
+    @JsonProperty("dax_metaserv_db")
+    val DAX_METASERV_DB: DataSourceFactory,
+
+    @JsonProperty("dax_db_default_user")
+    val DAX_DB_DEFAULT_USER: String,
+
+    @JsonProperty("dax_db_default_password")
+    val DAX_DB_DEFAULT_PASSWORD: String
+
+): Configuration()
