4316bed789653f96e6649da92d9d420fcc832719
Brian Van Klaveren <bvan@slac.stanford.edu>
2018-02-27 15:21:11 -0800

    Add sqlite database generation code

---
+++ src/main/kotlin/org/lsst/dax/albuquery/results/SqliteResult.kt
@@ -0,0 +1,44 @@
+package org.lsst.dax.albuquery.results
+
+import liquibase.Contexts
+import liquibase.Liquibase
+import liquibase.change.ColumnConfig
+import liquibase.change.core.CreateTableChange
+import liquibase.changelog.ChangeLogParameters
+import liquibase.changelog.DatabaseChangeLog
+import liquibase.database.DatabaseFactory
+import liquibase.parser.core.yaml.YamlChangeLogParser
+import liquibase.resource.FileSystemResourceAccessor
+import liquibase.resource.ResourceAccessor
+
+import org.lsst.dax.albuquery.JdbcColumnMetadata
+
+
+class SqliteResult {
+    companion object {
+        fun buildChangeLog(jdbcColumnMetadata: List<JdbcColumnMetadata>): DatabaseChangeLog {
+            val createTableChange = CreateTableChange()
+            createTableChange.tableName = "result"
+            for (column in jdbcColumnMetadata) {
+                val columnConfig = ColumnConfig()
+                columnConfig.name = column.name
+                columnConfig.type = column.jdbcType.toString()
+                createTableChange.addColumn(columnConfig)
+            }
+            val changeLogParameters = ChangeLogParameters()
+            val schemaResource = ::SqliteResult.javaClass.getResource("schema.yml")
+            val resourceAccessor: ResourceAccessor = FileSystemResourceAccessor()
+            val parser = YamlChangeLogParser()
+            val changeLog = parser.parse(schemaResource.file, changeLogParameters, resourceAccessor)
+            changeLog.changeSets[0].addChange(createTableChange)
+            return changeLog
+        }
+
+        fun initializeDatabase(changeLog: DatabaseChangeLog, dbUri: String){
+            val accessor : ResourceAccessor? = FileSystemResourceAccessor()
+            val liquibaseDb = DatabaseFactory.getInstance().openDatabase(dbUri,null, null, null, accessor)
+            val lb = Liquibase(changeLog, accessor, liquibaseDb)
+            lb.update(Contexts())
+        }
+    }
+}
---
+++ src/test/kotlin/org/lsst/dax/albuquery/results/SqliteResultsTest.kt
@@ -0,0 +1,31 @@
+package org.lsst.dax.albuquery.results
+
+import org.junit.Test
+import org.lsst.dax.albuquery.JdbcColumnMetadata
+import java.nio.file.Files
+import java.sql.JDBCType
+
+class SqliteResultsTest {
+
+    @Test
+    fun createDatabase() {
+        val columns = arrayListOf<JdbcColumnMetadata>()
+        columns.add(
+                JdbcColumnMetadata("foo", "result", 1, "integer", null, null, 1, JDBCType.BIGINT)
+        )
+        columns.add(
+                JdbcColumnMetadata("bar", "result", 1, "float", null, null, 1, JDBCType.FLOAT)
+        )
+        columns.add(
+                JdbcColumnMetadata("baz", "result", 1, "double", null, null, 1, JDBCType.DOUBLE)
+        )
+
+        val tmpdir = Files.createTempDirectory("sqlitetest")
+        val resultFilePath = tmpdir.resolve("test.db")
+        val sqliteUrl = "jdbc:sqlite:" + resultFilePath
+
+        val changeLog = SqliteResult.buildChangeLog(columns)
+        SqliteResult.initializeDatabase(changeLog, sqliteUrl)
+    }
+
+}
