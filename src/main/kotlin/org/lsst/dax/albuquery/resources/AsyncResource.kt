f51defc2b3f60b65661097f69f679263ec7e8101
Brian Van Klaveren <bvan@slac.stanford.edu>
2018-03-09 14:01:27 -0800

    Move createQuery code to companion object

--- src/main/kotlin/org/lsst/dax/albuquery/resources/AsyncResource.kt
+++ src/main/kotlin/org/lsst/dax/albuquery/resources/AsyncResource.kt
@@ -16,11 +16,12 @@ import org.lsst.dax.albuquery.tasks.QueryTask
 import java.net.URI
 import java.util.UUID
 import java.util.logging.Logger
-import javax.ws.rs.core.MediaType.APPLICATION_JSON
+import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Context
 import javax.ws.rs.core.Response
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.Future
+import javax.ws.rs.Consumes
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.POST
@@ -40,52 +41,20 @@ val DBURI = Regex("//.*")
 @Path("async")
 class AsyncResource(val metaservDAO: MetaservDAO) {
 
-    private val LOGGER = Logger.getLogger("AsyncResource")
     @Context
     lateinit var uri: UriInfo
-    val OUTSTANDING_QUERY_DATABASE = ConcurrentHashMap<String, Future<QueryTask>>()
 
+    @Timed
     @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     fun createQuery(query: String): Response {
-        val dbUri: URI
-        val queryStatement: Query
-        val qualifiedTables: List<ParsedTable>
-        try {
-            val statement = SqlParser().createStatement(query, ParsingOptions())
-            if (statement !is Query) {
-                val err = ErrorResponse("Only Select Queries allowed", "NotSelectStatementException", null, null)
-                return Response.status(Response.Status.BAD_REQUEST).entity(err).build()
-            }
-            val analyzer = Analyzer.TableAndColumnExtractor()
-            statement.accept(analyzer, null)
-            qualifiedTables = analyzer.tables
-            dbUri = Analyzer.getDatabaseURI(metaservDAO, analyzer.tables)
-            // Once we've found the database URI, rewrite the query
-            queryStatement = stripInstanceIdentifiers(statement)
-        } catch (ex: ParsingException) {
-            val err = ErrorResponse(ex.errorMessage, ex.javaClass.simpleName, null, cause = ex.message)
-            return Response.status(Response.Status.BAD_REQUEST).entity(err).build()
-        }
-
-        // FIXME: Assert firstTable is fully qualified to a known database
-        val queryId = UUID.randomUUID().toString()
-
-        // FIXME: Switch statement to support different types of tasks (e.g. MySQL, Qserv-specific)
-        val queryTask = QueryTask(metaservDAO, dbUri, queryId, queryStatement, qualifiedTables)
-
-        // FIXME: We're reasonably certain this will execute, execute a history task
-        val queryTaskFuture = EXECUTOR.submit(queryTask)
-
-        // FIXME: Use a real database (User, Monolithic?)
-        OUTSTANDING_QUERY_DATABASE[queryId] = queryTaskFuture
-        val createdUri = uri.requestUriBuilder.path(queryId).build()
-        return Response.seeOther(createdUri).build()
+        return createAsyncQuery(metaservDAO, uri, query, true)
     }
 
     @Timed
     @GET
-    @Path("{id}")
-    @Produces(APPLICATION_JSON)
+    @Path("{id}/results/result")
+    @Produces(MediaType.APPLICATION_JSON)
     fun getQuery(@PathParam("id") queryId: String): Response {
         val queryTaskFuture = findOutstandingQuery(queryId)
         if (queryTaskFuture != null) {
@@ -96,13 +65,65 @@ class AsyncResource(val metaservDAO: MetaservDAO) {
         return Response.status(Response.Status.NOT_FOUND).build()
     }
 
-    private fun stripInstanceIdentifiers(query: Query): Query {
-        // Rewrite query to extract database instance information
-        val rewriter = TableNameRewriter()
-        return rewriter.process(query) as Query
-    }
-
     private fun findOutstandingQuery(queryId: String): Future<QueryTask>? {
         return OUTSTANDING_QUERY_DATABASE[queryId]
+    }
+
+    companion object {
+        private val LOGGER = Logger.getLogger(::AsyncResource.name)
+        val OUTSTANDING_QUERY_DATABASE = ConcurrentHashMap<String, Future<QueryTask>>()
+
+        fun createAsyncQuery(
+            metaservDAO: MetaservDAO,
+            uri: UriInfo,
+            query: String,
+            resultRedirect: Boolean
+        ): Response {
+            val dbUri: URI
+            val queryStatement: Query
+            val qualifiedTables: List<ParsedTable>
+            try {
+                val statement = SqlParser().createStatement(query, ParsingOptions())
+                if (statement !is Query) {
+                    val err = ErrorResponse("Only Select Queries allowed", "NotSelectStatementException", null, null)
+                    return Response.status(Response.Status.BAD_REQUEST).entity(err).build()
+                }
+                val analyzer = Analyzer.TableAndColumnExtractor()
+                statement.accept(analyzer, null)
+                qualifiedTables = analyzer.tables
+                dbUri = Analyzer.getDatabaseURI(metaservDAO, analyzer.tables)
+                // Once we've found the database URI, rewrite the query
+                queryStatement = stripInstanceIdentifiers(statement)
+            } catch (ex: ParsingException) {
+                val err = ErrorResponse(ex.errorMessage, ex.javaClass.simpleName, null, cause = ex.message)
+                return Response.status(Response.Status.BAD_REQUEST).entity(err).build()
+            }
+
+            // FIXME: Assert firstTable is fully qualified to a known database
+            val queryId = UUID.randomUUID().toString()
+
+            // FIXME: Switch statement to support different types of tasks (e.g. MySQL, Qserv-specific)
+            val queryTask = QueryTask(metaservDAO, dbUri, queryId, queryStatement, qualifiedTables)
+
+            // FIXME: We're reasonably certain this will execute, execute a history task
+            val queryTaskFuture = EXECUTOR.submit(queryTask)
+
+            // FIXME: Use a real database (User, Monolithic?)
+            OUTSTANDING_QUERY_DATABASE[queryId] = queryTaskFuture
+
+            val createdUriBuilder = uri.requestUriBuilder.path(queryId)
+            val createdUri = if (resultRedirect) {
+                createdUriBuilder.path("results").path("result").build()
+            } else {
+                createdUriBuilder.build()
+            }
+            return Response.seeOther(createdUri).build()
+        }
+
+        private fun stripInstanceIdentifiers(query: Query): Query {
+            // Rewrite query to extract database instance information
+            val rewriter = TableNameRewriter()
+            return rewriter.process(query) as Query
+        }
     }
 }
