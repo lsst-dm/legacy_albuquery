e0fd106cac77d909086705ac32f51befd3487b65
Christine Banek <cbanek@lsst.org>
2018-08-28 23:38:17 -0500

    [DM-15571] Change namespace to dax-prod.
    This makes it the prod deployment, and I will move it to the prod
    folder in a following commit.

--- kube/create.sh
+++ kube/create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-DAX_NAMESPACE=${DAX_NAMESPACE:-'dax-develop'}
+DAX_NAMESPACE=${DAX_NAMESPACE:-'dax-prod'}
 
 kubectl create -f albuquery-deployment.yaml --namespace $DAX_NAMESPACE
 kubectl create -f albuquery-service.yaml --namespace $DAX_NAMESPACE
--- kube/create_secret.sh
+++ kube/create_secret.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
-DAX_NAMESPACE=${DAX_NAMESPACE:-'dax-develop'}
+DAX_NAMESPACE=${DAX_NAMESPACE:-'dax-prod'}
 
 kubectl create secret generic albuquery-config --from-file=./albuquery.yml --namespace $DAX_NAMESPACE
