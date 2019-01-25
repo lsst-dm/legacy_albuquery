a45430901ca53063de594e4b142afdeb498b6a42
Christine Banek <cbanek@lsst.org>
2019-01-25 14:59:29 -0600

    [DM-17456] Delete dev deployment.
    Going to directories in favor of the hostname (-int, -stable) and
    namespaces to match.

--- kube/dev/create.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -ex
-DAX_NAMESPACE=${DAX_NAMESPACE:-'dax-dev'}
-
-kubectl create -f albuquery-deployment.yaml --namespace $DAX_NAMESPACE
-kubectl create -f albuquery-service.yaml --namespace $DAX_NAMESPACE
-kubectl create -f albuquery-ingress.yaml --namespace $DAX_NAMESPACE
--- kube/dev/create_secret.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-DAX_NAMESPACE=${DAX_NAMESPACE:-'dax-dev'}
-
-kubectl create secret generic albuquery-config --from-file=./albuquery.yml --namespace $DAX_NAMESPACE
